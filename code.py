6838693efd53a3580fa9112a985d3bfc824622c2
Денчик <98116130+denischagin@users.noreply.github.com>
2022-01-30 00:43:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -24,14 +24,10 @@ def analysis(dict1):
     duplicate = {}
     for key1 in dict1:
         for key2 in dict1:
-            if key1 == key2:
-                pass
-            elif key1[key1.rfind('\\'):] == key2[key2.rfind('\\'):] and dict1[key1] == dict1[key2]:
-                if key1 in duplicate:
-                    continue
-                duplicate.update({key1: dict1[key1]})
-    else:
-        return duplicate
+            if key1[key1.rfind('\\'):] == key2[key2.rfind('\\'):] and dict1[key1] == dict1[key2] and key1 != key2:
+                if key1 not in duplicate:
+                    duplicate.update({key1: dict1[key1]})
+    return duplicate
 
 
 def duplicate(duplicate):
